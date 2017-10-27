b4d5255dcfde7316877116a8fd5b139cd41c1473
luis <luis@luis-K53TA>
2017-10-26 19:42:49 -0500

    Yo soy Luis Abraham Aquino Castillo 26/oct/17

--- Lenguajes/src/cuestionario/GeneradorCuestionario.java
+++ Lenguajes/src/cuestionario/GeneradorCuestionario.java
@@ -13,19 +13,23 @@ import java.util.List;
 public class GeneradorCuestionario {
     
     public ArrayList<Pregunta> generar(){
+        
         //Primero generaremos el arreglo que contrendra las preguntas
         ArrayList<Pregunta> preg= new ArrayList<Pregunta>();
         //Despues generaremos cada opcion de la primer pregunta
         Opcion opc1=new Opcion("Moscu",false);
         Opcion opc2=new Opcion("Kiev",false);
         Opcion opc3=new Opcion("Helsinki",true);
+        Opcion opc4=new Opcion("Chalco",false);
         
         ArrayList<Opcion> opcion1=new ArrayList<Opcion>();
         opcion1.add(opc1);
         opcion1.add(opc2);
+        opcion1.add(opc3);
+        opcion1.add(opc4);
         
-        Pregunta preg1=new Pregunta("Capital de Finlandia",opcion1);
-        preg.add(preg1);
+        Pregunta pregunta1=new Pregunta("Capital de Finlandia",opcion1);
+        preg.add(pregunta1);
         
         
         return preg;
